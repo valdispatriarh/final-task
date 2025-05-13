67793c74b29d56c57846d6a80ae4654c1084d84b
Vlad <valdispatriarh@gmail.com>
2025-05-13 22:56:02 +0200

    init

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ const baseConfig = {
 
 module.exports = ({ mode }) => {
     const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./final-task./webpack.dev.config');
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
     return merge(baseConfig, envConfig);
 };
